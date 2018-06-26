96643db6d17cb31738289a5d718ef5204bb90bac
Leonard Kioi <lenny.kinyanjui.lk@gmail.com>
2018-06-26 12:26:52 +0300

    add and configure css and sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,36 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   output: {
     path: path.resolve(__dirname, 'build', 'js'),
     filename: 'app.js'
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            "sass-loader" // compiles Sass to CSS
+        ]
+      }
+    ]
+  },
+  plugins: [
+      new MiniCssExtractPlugin({
+          // Options similar to the same options in webpackOptions.output
+          // both options are optional
+          filename: "[name].[hash].css",
+          path: path.resolve(__dirname, 'build', 'css')
+      })
+  ]
 };
