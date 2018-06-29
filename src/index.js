25284d8f10cf6f354e3a3c3da26c19d8d3c1da9c
Leonard Kioi <lenny.kinyanjui.lk@gmail.com>
2018-06-29 13:10:58 +0300

    create options separately

--- src/index.js
+++ src/index.js
@@ -7,20 +7,23 @@ function countriesCompareFunc(a, b) {
   return 0;
 }
 
+function createOptionFromCountry(country) {
+  const option = document.createElement('option');
+  option.text = country.currencyName;
+  option.value = country.currencyId;
+  return option;
+}
+
 function setCountries() {
   getCountries().then(response => response.json()).then((data) => {
     const countries = data.results;
     const fromSelect = document.querySelector('#app__from');
     const toSelect = document.querySelector('#app__to');
     const values = Object.values(countries);
-    const options = values.sort(countriesCompareFunc).map((value) => {
-      const option = document.createElement('option');
-      option.text = value.currencyName;
-      option.value = value.currencyId;
-      return option;
+    values.sort(countriesCompareFunc).forEach((value) => {
+      toSelect.appendChild(createOptionFromCountry(value));
+      fromSelect.appendChild(createOptionFromCountry(value));
     });
-    toSelect.append(...options);
-    fromSelect.append(...options);
   });
 }
 
